d3f975f77131e2788bf55aa7ef63e209e15a1c78
Abdul Rehman <46538502+Abdu-Snip@users.noreply.github.com>
2019-07-29 06:16:34 +0500

    Update Traingle formulae.py

--- Basic/Day 1/Traingle formulae.py
+++ Basic/Day 1/Traingle formulae.py
@@ -1,19 +1,19 @@
 while True:
-			base=int(input("Enter the length of triangle's base: "))
-			if(base>0):
-				break
-			else:
-				print('Enter a positive number')	
+	base=int(input("Enter the length of triangle's base: "))
+	if(base>0):
+		break
+	else:
+		print('Enter a positive number')	
 while True:        
-			height=int(input("Enter the length of triangle's height: "))
-			if(height>0):
-				break
-			else:
-				print('Enter a positive number')
+	height=int(input("Enter the length of triangle's height: "))
+	if(height>0):
+		break
+	else:
+		print('Enter a positive number')
 while True:    
-			hyp=int(input("Enter the length of triangle's third side: "))
-			if(hyp>0):
-				break
+	hyp=int(input("Enter the length of triangle's third side: "))
+	if(hyp>0):
+		break
 else: 
 	print('Enter a positive number')
 print('\nArea of triangle =',int(base*height),'\nPerimeter of triangle =',base+height+hyp)
